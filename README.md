4daba6df8060bc2d6197b3b7cdcec1c37e8a5477
Danny Diekroeger <dannydiek@gmail.com>
2021-02-24 14:59:43 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+### Find which seed phrase generates the given address
+
+Usage:
+
+First replace phrases in `phrases.txt` with the phrases you'd like to check. One per line separated by spaces.
+
+Then (replace the address below with an address controlled by the wallet):
+```
+npm i
+node check-seeds.js 33e69H1VfxZbYBZu2EcjUfv5thhV7CFN45
+```
---
+++ check-seeds.js
@@ -0,0 +1,45 @@
+const fs = require('fs')
+const bip39 = require('bip39')
+const bip32 = require('bip32')
+const bitcoin = require('bitcoinjs-lib')
+
+function run() {
+    const accounts = ['44', '49']
+    const phrases = readPhrases()
+    const depth = getDepth()
+    const targetAddress = getAddress()
+    for (const phrase of phrases) {
+        for (const account of accounts) {
+            for (let i = 0; i <= depth; i ++) {
+                const path = `m/${account}'/84'/0'/0'/0/${i}`
+                const seed = bip39.mnemonicToSeedSync(phrase)
+                const hdnode = bip32.fromSeed(seed)
+                const node = hdnode.derivePath(path)
+                const { address } = bitcoin.payments.p2sh({
+                    redeem: bitcoin.payments.p2wpkh({ pubkey: node.publicKey }),
+                });
+                if (address === targetAddress) {
+                    console.log(`Success with seed phrase:`)
+                    console.log(phrase)
+                    return
+                }
+            }
+        }
+    }
+    console.log('Did not find match')
+}
+
+function getDepth() {
+    return process.argv.length > 3 ? parseInt(process.argv[3]) : 50
+}
+
+function getAddress() {
+    if (!process.argv[2]) throw new Error("Please provide an address")
+    return process.argv[2]
+}
+
+function readPhrases() {
+    return fs.readFileSync('phrases.txt').toString().split('\n')
+}
+
+run()
